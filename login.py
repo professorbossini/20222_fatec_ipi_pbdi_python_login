c2a784d6cfc24a76b1c99ced8f85b4e648eb1102
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-06 10:26:22 -0300

    apostila07: fim da apostila

--- login.py
+++ login.py
@@ -19,7 +19,37 @@ def existe (usuario):
             #executa o comando
             cursor.execute(
                 'SELECT * FROM tb_usuario WHERE login=%s AND senha=%s',
-                usuario.login,
-                usuario.senha
+                (f'{usuario.login}', f'{usuario.senha}')    
             )
-    
+            #obter o resultado
+            result = cursor.fetchone()
+            # if result != None:
+            #     return True
+            # return False
+            # return True if result != None else False
+            return result != None
+
+#0 - Sair
+#1 - Login
+#2 - Logoff
+def menu():
+    texto = "0-Sair\n1-Login\n2-Logoff\n"
+    usuario = None
+    op = int(input(texto))
+    while op != 0:
+        if op == 1:
+            login = input('Digite seu login\n')
+            senha = input('Digite a sua senha\n')
+            usuario = Usuario(login, senha)
+            if existe(usuario):
+                print("Usuário OK!!!")
+            else:
+                print("Usuário NOK!!!")
+        elif op == 2:
+            usuario = None
+            print ("Logoff realizado com sucesso!!!")
+        op = int(input(texto))
+    else:
+        print ("Até mais")
+
+menu()
